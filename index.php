0ff121af92bdf3cacf866bbb9ac3ef7d0e4035a0
NParvedy <nicolasparvedy@gmail.com>
2019-12-24 03:51:00 +0100

    Page d'accueil réaliser

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Porfolio - Nicolas Parvedy</title>
+</head>
+<body>
+    <div id="advertisement">
+        <div class="advertisement_width">
+            <p>Je suis actuellement à la recherche d'un poste de "Developer back et front end" ! Venez en savoir plus sur mon <a href="#">parcours</a></p>
+            <img src="images/small_icons/cross-weight.png">
+        </div>
+    </div>
+
+    <header>
+        <div class="header_width">
+            <div id="header_top">
+                <ul>
+                    <li class="li_center">
+                        <img src="https://via.placeholder.com/32x32"> <a href="#"> Nicolas Parvedy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="small_frame_square">Voir ce projet</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="header_bottom">
+                <ul>
+                    <li class="medium_frame_square selected" >
+                        <h3>Client</h3>
+                        <p>Titre du projet sur 2 super grandes lignes</p>
+                    </li>
+
+                    <li class="medium_frame_square unselected">
+                        <h3>Client</h3>
+                        <p>Titre du projet sur 2 super grandes lignes</p>
+                    </li>
+
+                    <li class="medium_frame_square">
+                        <h3>Client</h3>
+                        <p>Titre du projet sur 2 super grandes lignes</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+    </header>
+
+    <div id="container_sort_by">
+        <div id="sort_by">
+            <div id="sort_by_left">
+                <p>Filtrer par techno :</p> 
+                <ul>
+                    <li><a href="#" class="small_frame_sqare_radius focus">Node.js</a></li>
+                    <li class="small_frame_sqare_radius"><a href="#">Vue.js</a></li>
+                    <li class="small_frame_sqare_radius"><a href="#">Php (symphony)</a></li>
+                </ul>
+            </div>
+
+            <div id="sort_by_right">
+                <p>Trier par : </p>
+                <ul class="small_frame_square_radius">
+                    <li><a href="#">Most recent first</a> <img src="images/small_icons/arrow-down-sign-to-navigate.png"></li>
+                </ul>
+            </div>
+            
+        </div>
+    </div>
+
+   
+
+    <section>
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+            
+        </div>
+
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+        </div>
+
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+        </div>
+
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+        </div>
+
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+        </div>
+
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+        </div>
+
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+        </div>
+
+        <div class="project">
+            <img src="https://via.placeholder.com/275x170">
+            <h3>Client</h3>
+            <p>Titre du projet sur 2 super grandes lignes</p>
+            <div class="project_icons">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+                <img src="https://via.placeholder.com/30x30">
+            </div>
+        </div>
+
+    </section>
+
+</body>
+</html>
